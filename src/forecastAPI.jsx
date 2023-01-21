7b2c2754feb6e2be67e17e77a1adcbe5c296aea8
Vikas Negi <negivikas201@gmail.com>
2023-01-21 13:48:51 +0530

    ver 2.0.0

--- src/forecastAPI.jsx
+++ src/forecastAPI.jsx
@@ -1,7 +1,5 @@
 const API_KEY = "250c2191a73d34573b8c4a88f88bf4e1";
 
-const makeIconURL = (iconId) =>
-  `https://openweathermap.org/img/wn/${iconId}@2x.png`;
 
 const getFormattedForecastData = async (city, units = "metric") => {
   const URL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=${units}`;
