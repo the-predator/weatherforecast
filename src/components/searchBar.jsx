4d37f74d620e78245ac99a5257ea314081f69892
Vikas Negi <negivikas201@gmail.com>
2023-01-21 13:56:37 +0530

    ver 2.0.0

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -6,8 +6,6 @@ function Search({city, handleChange, name}){
     <h1>Current Weather in {name}</h1>
     <div style={{display: 'flex', justifyContent: 'center'}}><input type="text" placeholder="Enter your City" onChange={handleChange} value={city} />
     </div>
-    
-    <h1></h1>
     </div>
     )
 }
